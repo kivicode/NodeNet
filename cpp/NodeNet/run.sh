25b72e9075366eeec9a0ef56de015bd1ac78c542
KIVICode <vova-ki-d@yandex.ru>
2020-08-03 15:34:10 +0300

    New stylesheet + proper custom docking

--- cpp/NodeNet/run.sh
+++ cpp/NodeNet/run.sh
@@ -1,1 +1,2 @@
+rm imgui.ini
 make node
--- cpp/NodeNet/src/main.cpp
+++ cpp/NodeNet/src/main.cpp
@@ -8,6 +8,74 @@
 #include <GL/gl3w.h>
 #include <dependencies/imgui-1.76/imgui_internal.h>
 
+void setStyle() {
+    ImGuiStyle &st = ImGui::GetStyle();
+    st.FrameBorderSize = 1.0f;
+    st.FramePadding = ImVec2(4.0f,2.0f);
+    st.ItemSpacing = ImVec2(8.0f,2.0f);
+    st.WindowBorderSize = 1.0f;
+    st.TabBorderSize = 1.0f;
+    st.WindowRounding = 1.0f;
+    st.ChildRounding = 1.0f;
+    st.FrameRounding = 1.0f;
+    st.ScrollbarRounding = 1.0f;
+    st.GrabRounding = 1.0f;
+    st.TabRounding = 1.0f;
+
+// Setup style
+    ImVec4* colors = ImGui::GetStyle().Colors;
+    colors[ImGuiCol_Text]                  = ImVec4(1.00f, 1.00f, 1.00f, 0.95f);
+    colors[ImGuiCol_TextDisabled]          = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+    colors[ImGuiCol_WindowBg]              = ImVec4(0.13f, 0.12f, 0.12f, 1.00f);
+    colors[ImGuiCol_ChildBg]               = ImVec4(1.00f, 1.00f, 1.00f, 0.00f);
+    colors[ImGuiCol_PopupBg]               = ImVec4(0.05f, 0.05f, 0.05f, 0.94f);
+    colors[ImGuiCol_Border]                = ImVec4(0.53f, 0.53f, 0.53f, 0.46f);
+    colors[ImGuiCol_BorderShadow]          = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+//    colors[ImGuiCol_FrameBg]               = ImVec4(0.00f, 0.00f, 0.00f, 0.85f);
+    colors[ImGuiCol_FrameBgHovered]        = ImVec4(0.22f, 0.22f, 0.22f, 0.40f);
+    colors[ImGuiCol_FrameBgActive]         = ImVec4(0.16f, 0.16f, 0.16f, 0.53f);
+    colors[ImGuiCol_TitleBg]               = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+    colors[ImGuiCol_TitleBgActive]         = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+    colors[ImGuiCol_TitleBgCollapsed]      = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
+    colors[ImGuiCol_MenuBarBg]             = ImVec4(0.12f, 0.12f, 0.12f, 1.00f);
+    colors[ImGuiCol_ScrollbarBg]           = ImVec4(0.02f, 0.02f, 0.02f, 0.53f);
+    colors[ImGuiCol_ScrollbarGrab]         = ImVec4(0.31f, 0.31f, 0.31f, 1.00f);
+    colors[ImGuiCol_ScrollbarGrabHovered]  = ImVec4(0.41f, 0.41f, 0.41f, 1.00f);
+    colors[ImGuiCol_ScrollbarGrabActive]   = ImVec4(0.48f, 0.48f, 0.48f, 1.00f);
+    colors[ImGuiCol_CheckMark]             = ImVec4(0.79f, 0.79f, 0.79f, 1.00f);
+    colors[ImGuiCol_SliderGrab]            = ImVec4(0.48f, 0.47f, 0.47f, 0.91f);
+    colors[ImGuiCol_SliderGrabActive]      = ImVec4(0.56f, 0.55f, 0.55f, 0.62f);
+    colors[ImGuiCol_Button]                = ImVec4(0.50f, 0.50f, 0.50f, 0.63f);
+    colors[ImGuiCol_ButtonHovered]         = ImVec4(0.67f, 0.67f, 0.68f, 0.63f);
+    colors[ImGuiCol_ButtonActive]          = ImVec4(0.26f, 0.26f, 0.26f, 0.63f);
+    colors[ImGuiCol_Header]                = ImVec4(0.54f, 0.54f, 0.54f, 0.58f);
+    colors[ImGuiCol_HeaderHovered]         = ImVec4(0.64f, 0.65f, 0.65f, 0.80f);
+    colors[ImGuiCol_HeaderActive]          = ImVec4(0.25f, 0.25f, 0.25f, 0.80f);
+    colors[ImGuiCol_Separator]             = ImVec4(0.58f, 0.58f, 0.58f, 0.50f);
+    colors[ImGuiCol_SeparatorHovered]      = ImVec4(0.81f, 0.81f, 0.81f, 0.64f);
+    colors[ImGuiCol_SeparatorActive]       = ImVec4(0.81f, 0.81f, 0.81f, 0.64f);
+    colors[ImGuiCol_ResizeGrip]            = ImVec4(0.87f, 0.87f, 0.87f, 0.53f);
+    colors[ImGuiCol_ResizeGripHovered]     = ImVec4(0.87f, 0.87f, 0.87f, 0.74f);
+    colors[ImGuiCol_ResizeGripActive]      = ImVec4(0.87f, 0.87f, 0.87f, 0.74f);
+    colors[ImGuiCol_Tab]                   = ImVec4(0.01f, 0.01f, 0.01f, 0.86f);
+    colors[ImGuiCol_TabHovered]            = ImVec4(0.29f, 0.29f, 0.29f, 1.00f);
+    colors[ImGuiCol_TabActive]             = ImVec4(0.31f, 0.31f, 0.31f, 1.00f);
+    colors[ImGuiCol_TabUnfocused]          = ImVec4(0.02f, 0.02f, 0.02f, 1.00f);
+    colors[ImGuiCol_TabUnfocusedActive]    = ImVec4(0.19f, 0.19f, 0.19f, 1.00f);
+    colors[ImGuiCol_DockingPreview]        = ImVec4(0.38f, 0.48f, 0.60f, 1.00f);
+    colors[ImGuiCol_DockingEmptyBg]        = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
+    colors[ImGuiCol_PlotLines]             = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
+    colors[ImGuiCol_PlotLinesHovered]      = ImVec4(0.68f, 0.68f, 0.68f, 1.00f);
+    colors[ImGuiCol_PlotHistogram]         = ImVec4(0.90f, 0.77f, 0.33f, 1.00f);
+    colors[ImGuiCol_PlotHistogramHovered]  = ImVec4(0.87f, 0.55f, 0.08f, 1.00f);
+    colors[ImGuiCol_TextSelectedBg]        = ImVec4(0.47f, 0.60f, 0.76f, 0.47f);
+    colors[ImGuiCol_DragDropTarget]        = ImVec4(0.58f, 0.58f, 0.58f, 0.90f);
+    colors[ImGuiCol_NavHighlight]          = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
+    colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
+    colors[ImGuiCol_NavWindowingDimBg]     = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
+    colors[ImGuiCol_ModalWindowDimBg]      = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
+}
+
 
 int main(int, char**)
 {
@@ -95,6 +163,7 @@ int main(int, char**)
 		}
 
 		// Start the Dear ImGui frame
+         setStyle();
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
@@ -107,32 +176,21 @@ int main(int, char**)
 		ImGui::NewFrame();
 
 
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar;
+
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+        ImGui::SetNextWindowSize(ImGui::GetWindowViewport()->GetWorkSize());
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::Begin("Dockspace", nullptr, window_flags);
+        ImGui::PopStyleVar(1);
+
+
+        ImGuiID dockspace_id = ImGui::GetID("Dockspace");
+        ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+
         if (!dockspaceBuilt) {
             ImGuiViewport *viewport = ImGui::GetMainViewport();
-            ImGui::SetNextWindowPos(viewport->Pos);
-            ImGui::SetNextWindowSize(viewport->Size);
-            ImGui::SetNextWindowViewport(viewport->ID);
-            ImGui::SetNextWindowBgAlpha(0.0f);
-
-            ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize |
-                            ImGuiWindowFlags_NoMove;
-            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
-            bool p_open = true;
-            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-            ImGui::Begin("Dockspace", &p_open, window_flags);
-
-            ImGui::BeginChild("", ImVec2(0,0), true, ImGuiWindowFlags_NoMove);
-            ImGui::PopStyleVar(3);
-
-            ImGuiID dockspace_id = ImGui::GetID("Dockspace");
-            ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
-            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-
-
             ImGui::DockBuilderRemoveNode(dockspace_id); // Clear out existing layout
             ImGui::DockBuilderAddNode(dockspace_id, dockspace_flags); // Add empty node
             ImGui::DockBuilderSetNodeSize(dockspace_id, {viewport->GetWorkSize().x, viewport->GetWorkSize().y});
@@ -149,13 +207,11 @@ int main(int, char**)
             ImGui::DockBuilderDockWindow("Inspector",     dock_id_left);   // left
             ImGui::DockBuilderDockWindow("Configuration", dock_id_right);  // right
             ImGui::DockBuilderFinish(dockspace_id);
-
-            ImGui::DockBuilderFinish(dockspace_id);
-
-            ImGui::EndChild();
-            ImGui::End();
             dockspaceBuilt = true;
         }
+
+        ImGui::End();
+        
 
 		if (!initialized)
 		{
